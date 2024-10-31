cdedcbbc11ad3e5fa1e5a514e810f94be12f1f97
Abhishek M <abhi.24.2002@gmail.com>
2024-10-31 19:54:00 +0530

    removed cyl.jsx and integrated in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,37 @@
-import {Canvas} from "@react-three/fiber";
-import React from 'react';
+import React, { useRef } from 'react'
+import {Canvas , useFrame , useLoader} from "@react-three/fiber";
 import "./style.css";
-import { OrbitControls } from "@react-three/drei";
-import Cyl from "./Cyl";
+import { OrbitControls , useTexture } from "@react-three/drei";
 import { Bloom, EffectComposer, ToneMapping } from "@react-three/postprocessing";
+import * as THREE from "three";
+import { TextureLoader } from 'three/src/loaders/TextureLoader';
+
+function Cyl() {
+  let cyl = useRef(null);
+  const tex = useLoader(
+    TextureLoader,
+    './images.png'
+  );
+
+
+  useFrame((state, delta) => {
+    cyl.current.rotation.y += delta;
+  });
+
+  return (
+    <group rotation={[0, 1.5, 0.5]}>
+    <mesh ref={cyl} >
+  
+    <cylinderGeometry args={[1,1,1,60,60,true]} />
+
+    <meshStandardMaterial map={tex} transparent side={THREE.DoubleSide} />
+
+  </mesh>
+  </group>
+  );
+}
+
+
 
 const App = () => {
   
@@ -18,9 +46,9 @@ const App = () => {
     <EffectComposer>
     <Bloom
     mipmapBlur // Enables or disables mipmap blur.
-    intensity={25.0} // The bloom intensity.
-    luminanceThreshold={0} // luminance threshold. Raise this value to mask out darker elements in the scene.
-    luminanceSmoothing={0.5} // smoothness of the luminance threshold. Range is [0, 1]
+    intensity={8.0} // The bloom intensity.
+    luminanceThreshold={0.2} // luminance threshold. Raise this value to mask out darker elements in the scene.
+    luminanceSmoothing={0.7} // smoothness of the luminance threshold. Range is [0, 1]
     />
    </EffectComposer>
     </Canvas>
